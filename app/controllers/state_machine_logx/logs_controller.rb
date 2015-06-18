93c94ff974a1c8523721af471c2145ac9635da83
jc <emclab2011@gmail.com>
2015-06-17 22:12:29 -0500

    JC: added bootstrap. upgraded views to rails 4 & boot. removed gem protected_attributes. spec passes.

--- app/controllers/state_machine_logx/logs_controller.rb
+++ app/controllers/state_machine_logx/logs_controller.rb
@@ -17,5 +17,10 @@ module StateMachineLogx
       @log = StateMachineLogx::Log.find_by_id(params[:id])
       @erb_code = find_config_const('log_show_view', 'state_machine_logx')
     end
+    
+    private
+    def new_params
+      params.require(:log).permit(:action_by_name, :comment, :error_message, :event, :from, :resource_string, :resource_id, :to, :last_updated_by_id)
+    end
   end
 end
--- app/models/state_machine_logx/log.rb
+++ app/models/state_machine_logx/log.rb
@@ -1,6 +1,6 @@
 module StateMachineLogx
   class Log < ActiveRecord::Base
-    attr_accessible :action_by_name, :comment, :error_message, :event, :from, :resource_string, :resource_id, :to, :last_updated_by_id, :as => :role_new
+    #attr_accessible :action_by_name, :comment, :error_message, :event, :from, :resource_string, :resource_id, :to, :last_updated_by_id, :as => :role_new
     
     belongs_to :last_updated_by, :class_name => 'Authentify::User'
     
